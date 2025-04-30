bd736efa7a492ae53e47efcd95f055edaf8fff02
Thorsten Seyschab <business@todde.tv>
2025-05-01 00:08:17 +0200

    feat: add empty main script file

---
+++ main.py
@@ -0,0 +1,13 @@
+def main():
+    print_header()
+
+
+def print_header():
+    line_length = 50
+    print("=" * line_length)
+    print("Automated Computer Setup".center(line_length))
+    print("=" * line_length)
+
+
+if __name__ == "__main__":
+    main()
