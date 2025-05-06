ca4aebb1a74ece7e2784b5d4349a9970e08616f8
Thorsten Seyschab <business@todde.tv>
2025-05-06 12:41:20 +0200

    feat: add variable checks to subscript skeleton init

--- subscript_skeleton.py
+++ subscript_skeleton.py
@@ -13,8 +13,21 @@ class SubscriptSkeleton(ABC):
     def __init__(self, prefix_color: COLOR):
         if not hasattr(self, "name_short"):
             raise ValueError("Sub-scripts must define a 'name_short' attribute.")
+        if not self.name_short:
+            raise ValueError("name_short must not be empty")
+        if len(self.name_short) > 30:
+            raise ValueError("name_short must be 30 characters or less")
+
         if not hasattr(self, "name_long"):
             raise ValueError("Sub-scripts must define a 'name_long' attribute.")
+        if not self.name_long:
+            raise ValueError("name_long must not be empty")
+
+        if self.name_short == self.name_long:
+            raise ValueError("name_short and name_long must be different")
+
+        if not isinstance(prefix_color, COLOR):
+            raise TypeError("prefix_color must be an instance of COLOR Enum")
 
         self.prefix_name_color = prefix_color
 
