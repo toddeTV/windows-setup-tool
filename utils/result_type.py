971ad806cc2b1ebcfe2b9fc2e6ae37071df4ff91
Thorsten Seyschab <business@todde.tv>
2025-05-06 08:47:42 +0200

    feat: extend subscript result types

--- utils/result_type.py
+++ utils/result_type.py
@@ -2,7 +2,10 @@ from enum import Enum
 
 
 class SubScriptResult(Enum):
-    SUCCESS = "success"
-    ABORT_SOFT = "abort_soft"
-    ABORT_HARD = "abort_hard"
-    ABORT_EXCEPTION = "abort_exception"
+    SUCCESS = "success"  # successful completion of a subscript
+    ABORT_SOFT = (
+        "abort_soft"  # soft abort from a subscript, rest of the program should continue
+    )
+    ABORT_HARD = "abort_hard"  # hard abort from a subscript, rest of the program should be aborted
+    ABORT_EXCEPTION = "abort_exception"  # exception/error occurred
+    ABORT_USER = "abort_user"  # interrupted by user (Ctrl+C)
