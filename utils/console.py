92934d7751630e8f1884b80b5c4f2d211cb92b6f
Thorsten Seyschab <business@todde.tv>
2025-05-10 11:50:20 +0200

    feat: add logging to console prints

--- utils/console.py
+++ utils/console.py
@@ -1,21 +1,6 @@
 from colorama import init, Fore, Style
 from enum import Enum
-
-
-def init_colorama():
-    """
-    Initializes `colorama` for cross-platform compatibility.
-    This function should be called at the start of the program to ensure
-    that ANSI escape sequences are interpreted correctly on Windows.
-    """
-    # Check if `colorama` is already initialized & ensure it's only initialized once
-    if not hasattr(init_colorama, "initialized"):
-        print_with_prefix_main("Initializing 'colorama'.")
-        init_colorama.initialized = True
-        init(autoreset=True)
-    else:
-        # If already initialized, do nothing
-        return
+from utils.logs import LogLevelEnum, Logs
 
 
 class COLOR(Enum):
@@ -30,6 +15,24 @@ class COLOR(Enum):
     MAGENTA = Fore.MAGENTA
     RED = Fore.RED
     YELLOW = Fore.YELLOW
+
+
+def init_colorama():
+    """
+    Initializes `colorama` for cross-platform compatibility.
+    This function should be called at the start of the program to ensure
+    that ANSI escape sequences are interpreted correctly on Windows.
+    """
+    # Check if `colorama` is already initialized & ensure it's only initialized once
+    if not hasattr(init_colorama, "initialized"):
+        print_with_prefix_main(
+            log_level=LogLevelEnum.DEBUG, text="Initializing 'colorama'."
+        )
+        init_colorama.initialized = True
+        init(autoreset=True)
+    else:
+        # If already initialized, do nothing
+        return
 
 
 def colorize(text: str, color: COLOR) -> str:
@@ -48,16 +51,39 @@ def colorize(text: str, color: COLOR) -> str:
     return f"{color.value}{text}{COLOR.RESET_ALL.value}"
 
 
-def print_with_prefix(prefix: str, color: COLOR, *args, **kwargs):
+# def print_with_prefix(prefix_text: str, prefix_color: COLOR, *args, **kwargs):
+#     text: str = " ".join(map(str, args))
+#     print(text, **kwargs)
+def print_with_prefix(
+    prefix_text: str,
+    prefix_color: COLOR,
+    text: str = "",
+    log_level: LogLevelEnum = LogLevelEnum.INFO,
+):
     """Prints the message with the main identifier as prefix."""
-    text: str = " ".join(map(str, args))
+
     if text == "":
         print()
+        Logs().log(level=log_level, message="")
         return
-    prefix_modified: str = f"[{colorize(prefix.center(30), color)}] "
-    print(f"{prefix_modified}{text}", **kwargs)
+
+    # prefix_text_stretched: str = prefix_text.center(30)
+    prefix_text_stretched: str = prefix_text.ljust(30)
+
+    prefix_modified_color_yes: str = (
+        f"[{colorize(prefix_text_stretched, prefix_color)}] "
+    )
+    prefix_modified_color_no: str = f"[{prefix_text_stretched}] "
+
+    output_color_yes: str = f"{prefix_modified_color_yes}{text}"
+    output_color_no: str = f"{prefix_modified_color_no}{text}"
+
+    print(output_color_yes)
+    Logs().log(level=log_level, message=output_color_no)
 
 
-def print_with_prefix_main(*args, **kwargs):
+def print_with_prefix_main(text: str = "", log_level: LogLevelEnum = LogLevelEnum.INFO):
     """Prints the message with the main identifier as prefix."""
-    print_with_prefix("main", COLOR.MAGENTA, *args, **kwargs)
+    print_with_prefix(
+        prefix_text="main", prefix_color=COLOR.MAGENTA, text=text, log_level=log_level
+    )
