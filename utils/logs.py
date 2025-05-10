a9a55110d2f804b311b560346c28813af7b433f6
Thorsten Seyschab <business@todde.tv>
2025-05-10 11:49:05 +0200

    feat: add Logger class as Singleton

---
+++ utils/logs.py
@@ -0,0 +1,55 @@
+import logging
+from utils.singleton import Singleton
+from datetime import datetime
+from enum import Enum
+
+
+class LogLevelEnum(Enum):
+    DEBUG = logging.DEBUG
+    INFO = logging.INFO
+    WARNING = logging.WARNING
+    ERROR = logging.ERROR
+    # CRITICAL = logging.CRITICAL
+    # FATAL = logging.FATAL
+
+
+# from typing import Literal
+# LogLevelType = Literal[LogLevelEnum.ERROR, LogLevelEnum.WARNING]
+
+
+class Logs(metaclass=Singleton):
+    """
+    Singleton class for logging.
+    This class is used to log messages to a file and the console.
+    It ensures that only one instance of the logger is created and used throughout the application.
+    """
+
+    _log_path: str = "logs"
+    _filename: str = datetime.today().strftime("%Y-%m-%d_%H-%M-%S.log")
+    _full_path: str = f"{_log_path}/{_filename}"
+
+    logging.basicConfig(
+        level=logging.DEBUG,
+        filename=_full_path,
+        filemode="a+",
+        # filemode="w",
+        encoding="utf-8",
+        format="%(asctime)-15s %(levelname)-8s %(message)s",
+    )
+
+    _logger: logging.Logger = logging.getLogger()
+
+    def log(self, level: LogLevelEnum, message: str) -> None:
+        """
+        Logs a message with the specified log level.
+        Args:
+            level (LogLevelEnum): The log level to use.
+            message (str): The message to log.
+        """
+
+        if not isinstance(level, LogLevelEnum):
+            raise TypeError("level must be an instance of LogLevelEnum")
+        if not isinstance(message, str):
+            raise TypeError("message must be a string")
+
+        self._logger.log(level.value, message)
